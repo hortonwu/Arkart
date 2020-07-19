6f553c4460c187e7dcddaecac5eee039334e87a4
hortonwu <haoitan-wu@outlook.com>
2020-07-18 23:38:46 -0700

    add video recording for arkart app and improve forward and reverse drive

--- source/RaspberryPi/Arkart.py
+++ source/RaspberryPi/Arkart.py
@@ -7,9 +7,12 @@ import distanceHCSR
 import time
 import opcua_server
 import accgyro9dof
+import cameraPi
+# from datetime import datetime
 
 server=opcua_server.server_setup()
 
+# constant configurations 
 front_s = constant.FRONT_STEER
 rear_s = constant.REAR_STEER
 zero_s = constant.ZERO_SPD
@@ -20,6 +23,10 @@ dead_h = constant.DEAD_H
 rear_a = constant.REAR_A
 front_rw = constant.FRONT_RW
 front_lw = constant.FRONT_LW
+#camera rotation
+rotation =270
+directory_pic = '/home/pi/Desktop/Arkart_image.jpg'
+directory_video = '/home/pi/Desktop/Arkart_video.h264'
 
 #front servo position 105 degree
 #clockwise circle foward
@@ -31,7 +38,7 @@ steer_rr = 56
 
 #target motor speed
 target_sf= 100
-target_sr= 88
+target_sr= 87
 brake_sf=95
 
 obstacle_dist = 40
@@ -40,12 +47,15 @@ if __name__ == '__main__':
 #         val_n = 0
 #         val_n1 = 0
 #         val_n2 =0
+        # switch 0 and 1 to for one time reset
+        switch = True
         # previous step velocity 
         velocity_n1= 0.0
         #motor drive duration
-        time_T = 0.05
-        time_Tbf = 0.3
-        time_Tb = 0.05
+        time_Tf = 0.1
+        time_Tr = 0.5
+        time_Tbf = 0.25
+        time_Tbr = 0.05
         #rear axle previous speed
         spd_n1_ra = target_sf
         #rear axle previous speed
@@ -62,6 +72,9 @@ if __name__ == '__main__':
 #         servos16ch.Motor_Spd_Init(front_lw, zero_s, test_s)
         #ACC and GYRO
         fxos_fxas=accgyro9dof.Accgyro_Init()
+        camera = cameraPi.CameraInit(rotation)
+#         camera.annotate_text = "Hello Jing, The Arkart is circuling, Enjoy!!!-{0:%Y}-{0:%m}-{0:%d}".format(datetime.now())
+        cameraPi.Video(True,directory_video,camera)
         while True:
             dist = distanceHCSR.distance()
             #movingAverage(dist,val_n1,val_n2,val_n)
@@ -80,16 +93,16 @@ if __name__ == '__main__':
             spd_n1_ra = servos16ch.Motor_Spd_Set(rear_a,spd_n1_ra,target_sf,dead_l,dead_h)
             acc_n1=accgyro9dof.Acc_Read(fxos_fxas[0])
             
-            time.sleep(time_T)        
+            time.sleep(time_Tf)        
 
             acc_n=accgyro9dof.Acc_Read(fxos_fxas[0])
-            velocity_n = velocity_n1 + (acc_n[0]+acc_n1[0])*time_T/2.0 
+            velocity_n = velocity_n1 + (acc_n[0]+acc_n1[0])*time_Tf/2.0 
             velocity_n1 = velocity_n
             acc_n1=acc_n
             
             while (velocity_n < -0.4) or (acc_n[0]<-1.2): #(acc[0] < -0.4) or (acc[1] < -0.4):
                 spd_n1_ra = servos16ch.Motor_Spd_Set(rear_a,spd_n1_ra,brake_sf,dead_l,dead_h)
-                print("Forward too fast, Brake! Acc_X: %0.3f, velocity:%0.3f" % (acc_n[0],velocity_n))
+                print("Forward too fast, Brake! Acc_X: %0.3f, Acc_Y:%0.3f velocity:%0.3f" % (acc_n[0],acc_n[1],velocity_n))
                 time.sleep(time_Tbf)
                 acc_n=accgyro9dof.Acc_Read(fxos_fxas[0])
                 velocity_n = velocity_n1 + (acc_n[0]+acc_n1[0])*time_Tbf/2 
@@ -115,19 +128,19 @@ if __name__ == '__main__':
     #             spd_n1_fl = servos16ch.Motor_Spd_Set(front_lw,spd_n1_fl,target_sr,dead_l,dead_h)
 #                 acc_n1=accgyro9dof.Acc_Read(fxos_fxas[0])
             
-                time.sleep(time_T)        
+                time.sleep(time_Tr)        
 
                 acc_n=accgyro9dof.Acc_Read(fxos_fxas[0])
-                velocity_n = velocity_n1 + (acc_n[0]+acc_n1[0])*time_T*0.5 
+                velocity_n = velocity_n1 + (acc_n[0]+acc_n1[0])*time_Tr*0.5 
                 velocity_n1 = velocity_n
                 acc_n1=acc_n
                 
                 while velocity_n > 0.8 :#(acc[0] < -0.4) or (acc[1] < -0.4):
                     spd_n1_ra = servos16ch.Motor_Spd_Set(rear_a,spd_n1_ra,zero_s,dead_l,dead_h)
                     print("Reverse too fast, Brake! Acc_X: %0.3f, velocity:%0.3f" % (acc_n[0],velocity_n))
-                    time.sleep(time_Tb)
+                    time.sleep(time_Tbr)
                     acc_n=accgyro9dof.Acc_Read(fxos_fxas[0])
-                    velocity_n = velocity_n1 + (acc_n[0]+acc_n1[0])*time_Tb*0.5
+                    velocity_n = velocity_n1 + (acc_n[0]+acc_n1[0])*time_Tbr*0.5
                     velocity_n1 = velocity_n
                     acc_n1=acc_n
                     if acc_n1[0]<0.5:
@@ -136,6 +149,9 @@ if __name__ == '__main__':
                 dist = distanceHCSR.distance()
                 print ("Measured Distance = %.1f cm" % dist)
                 acc_n1=(0.0,0.0,0.0)
+                if switch is True:
+                    switch = False
+                    cameraPi.Video(False,directory_video,camera)
     
                 
  
